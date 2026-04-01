80f73a6a1f870305d436adcf17648221c7c8f636
Kaia Scheirman <kfscheirman18@gmail.com>
2026-04-01 13:04:48 -0700

    tweak redirectTo

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -79,7 +79,7 @@ export default function LoginPage() {
       const { error: resetError } = await supabase.auth.resetPasswordForEmail(
         email,
         {
-          redirectTo: `${window.location.origin}/auth/callback?next=/reset-password`,
+          redirectTo: window.location.origin,
         },
       );
       if (resetError) {
